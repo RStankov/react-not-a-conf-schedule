5b2f8f3cddb2f83a1f3c04aee00edfe3fca36e5e
Radoslav Stankov <rstankov@gmail.com>
2018-04-05 19:01:24 +0300

    Set positions

--- src/App.js
+++ src/App.js
@@ -1,48 +1,61 @@
 import React from 'react';
 import ScheduleTable from './ScheduleTable';
 
-const TALKS = [{
-  id: 1,
-  name: '🤔😅⁇',
-}, {
-  id: 2,
-  name: 'Reactive Blockchain',
-}, {
-  id: 3,
-  name: 'Opening',
-}, {
-  id: 4,
-  name: 'NADCast',
-}, {
-  id: 5,
-  name: 'Integrating React Into Legacy Projects',
-}, {
-  id: 6,
-  name: 'Rebuilding Product Hunt Mobile with React.Native',
-}, {
-  id: 7,
-  name: 'Designing with React',
-}, {
-  id: 8,
-  name: 'Garbage Collection in Modern Web Applications',
-}, {
-  id: 10,
-  name: 'Humanizing Your Documentation',
-}, {
-  id: 11,
-  name: 'Rebecca Hill',
-}, {
-  id: 12,
-  name: 'Lunch Break',
-}, {
-  id: 13,
-  name: 'Coffee Break',
-}]
-
+const TALKS = [
+  {
+    id: 1,
+    name: '🤔😅⁇',
+  },
+  {
+    id: 2,
+    name: 'Reactive Blockchain',
+  },
+  {
+    id: 3,
+    name: 'Opening',
+  },
+  {
+    id: 4,
+    name: 'NADCast',
+  },
+  {
+    id: 5,
+    name: 'Integrating React Into Legacy Projects',
+  },
+  {
+    id: 6,
+    name: 'Rebuilding Product Hunt Mobile with React.Native',
+  },
+  {
+    id: 7,
+    name: 'Designing with React',
+  },
+  {
+    id: 8,
+    name: 'Garbage Collection in Modern Web Applications',
+  },
+  {
+    id: 10,
+    name: 'Humanizing Your Documentation',
+  },
+  {
+    id: 11,
+    name: 'Rebecca Hill',
+  },
+  {
+    id: 12,
+    name: 'Lunch Break',
+  },
+  {
+    id: 13,
+    name: 'Coffee Break',
+  },
+];
+const START_AT = '09:45';
 export default function App() {
   return (
     <main>
-      <ScheduleTable talks={TALKS} />
+      <ScheduleTable talks={TALKS} startAt={START_AT} />
     </main>
   );
 }
--- src/ScheduleTable/index.js
+++ src/ScheduleTable/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { sortBy, last } from 'lodash';
 
 export default class ScheduleTable extends Component {
   state = {
-    talks: this.props.talks.map(talk => ({
+    talks: this.props.talks.map((talk, i) => ({
       ...talk,
       startAt: '00:00',
       length: '30',
@@ -13,11 +14,20 @@ export default class ScheduleTable extends Component {
   updateTalk = newTalk => {
     console.log(newTalk);
     this.setState({
-      talks: this.state.talks.map(
-        talk => (talk.id === newTalk.id ? newTalk : talk),
+      talks: sortBy(
+        this.state.talks.map(talk => (talk.id === newTalk.id ? newTalk : talk)),
+        'position',
       ),
     });
   };
+
+  move(x, y) {
+    return () => {
+      this.setState({
+        talks: swap(this.state.talks, x, y),
+      });
+    };
+  }
 
   render() {
     return (
@@ -26,6 +36,7 @@ export default class ScheduleTable extends Component {
           <tr>
             <th>Id</th>
             <th>Name</th>
+            <th />
             <th>Length</th>
             <th>QA</th>
             <th>Start at</th>
@@ -33,13 +44,17 @@ export default class ScheduleTable extends Component {
           </tr>
         </thead>
         <tbody>
-          {this.state.talks.map(talk =>
+          {this.state.talks.map((talk, i) =>
             <tr key={talk.id}>
               <td>
                 {talk.id}
               </td>
               <td>
                 {talk.name}
+              </td>
+              <td>
+                <button onClick={this.move(i - 1, i)}>⬆</button>
+                <button onClick={this.move(i, i + 1)}>⬇</button>
               </td>
               <td>
                 <TextInput
@@ -58,13 +73,34 @@ export default class ScheduleTable extends Component {
                   name="startAt"
                 />
               </td>
-              <td>00:00</td>
+              <td>
+                {calculateFinishAt(talk)}
+              </td>
             </tr>,
           )}
         </tbody>
       </table>
     );
   }
+}
+
+function calculateFinishAt(talk) {
+  return parseInt(talk.length, 10) + parseInt(talk.qa, 10);
+}
+
+function swap(a, x, y) {
+  [x, y] = [x, y].sort();
+  return (
+    (a[x] &&
+    a[y] && [
+      ...a.slice(0, x),
+      a[y],
+      ...a.slice(x + 1, y),
+      a[x],
+      ...a.slice(y + 1),
+    ]) ||
+    a
+  );
 }
 
 class TextInput extends Component {
