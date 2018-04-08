f6385fb1ad7de9f231b7c72aa45d42122f2a1f44
Radoslav Stankov <rstankov@gmail.com>
2018-04-08 23:59:50 +0300

    Arrange talks

--- src/App.js
+++ src/App.js
@@ -3,36 +3,16 @@ import ScheduleTable from './ScheduleTable';
 
 const TALKS = [
   {
-    id: 1,
-    name: '🤔😅⁇',
-  },
-  {
-    id: 2,
-    name: 'Reactive Blockchain',
-  },
-  {
     id: 3,
     name: 'Opening',
-  },
-  {
-    id: 4,
-    name: 'NADCast',
   },
   {
     id: 5,
     name: 'Integrating React Into Legacy Projects',
   },
   {
-    id: 6,
-    name: 'Rebuilding Product Hunt Mobile with React.Native',
-  },
-  {
-    id: 7,
-    name: 'Designing with React',
-  },
-  {
-    id: 8,
-    name: 'Garbage Collection in Modern Web Applications',
+    id: 2,
+    name: 'Reactive Blockchain',
   },
   {
     id: 10,
@@ -40,15 +20,39 @@ const TALKS = [
   },
   {
     id: 11,
-    name: 'Rebecca Hill',
+    name: 'Sagas and Selectors',
   },
   {
     id: 12,
     name: 'Lunch Break',
   },
   {
+    id: 4,
+    name: 'NADCast',
+  },
+  {
+    id: 8,
+    name: 'Garbage Collection in Modern Web Applications',
+  },
+  {
+    id: 7,
+    name: 'Designing with React',
+  },
+  {
     id: 13,
     name: 'Coffee Break',
+  },
+  {
+    id: 6,
+    name: 'Rebuilding Product Hunt Mobile with React.Native',
+  },
+  {
+    id: 1,
+    name: '🤔😅⁇',
+  },
+  {
+    id: 14,
+    name: 'Closing',
   },
 ];
 const START_AT = '09:45';
